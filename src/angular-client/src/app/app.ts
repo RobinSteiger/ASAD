389b8d670d05c424dd49cda26b9de07606c99bd7
Yao N'DRI <90345080+AnassNDRI@users.noreply.github.com>
2026-05-24 00:10:10 +0200

    Code Review

--- src/angular-client/src/app/app.ts
+++ src/angular-client/src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {provideIcons} from '@ng-icons/core';
-import {heroXCircle} from '@ng-icons/heroicons/outline';
-
+import { provideIcons } from '@ng-icons/core';
+import { heroXCircle } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ],
+  imports: [RouterOutlet],
   viewProviders: [provideIcons({ heroXCircle })],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-client');
